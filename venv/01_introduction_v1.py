54249efb3b1177c154a38ee5b2f96b25c1f513b5
magno.franc <magno.franc@awatapu.school.nz>
2021-09-15 15:01:32 +1200

    introduction to game

---
+++ venv/01_introduction_v1.py
@@ -0,0 +1,55 @@
+# functions go here
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input("Have you played "
+                         "the game before?").lower()
+
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        # If input no, display instructions
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        #If input not yes or no, clarify question
+        else:
+            print("<error> please insert a yes / no")
+
+def want_instructions(question):
+    valid = False
+    while not valid:
+        response = input("Do you want to see the instructions?").lower()
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        # If input no, display instructions
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        #If input not yes or no, clarify question
+        else:
+            print("<error> please insert a yes / no")
+
+# main program goes here
+print("Welcome to the Timer Quiz Game :]")
+played_before = yes_no("Have you played before?")
+if played_before == "no":
+    instructions = want_instructions("Do you want to see the instructions?")
+    if instructions == "no":
+        print()
+    elif instructions == "yes":
+        print("instructions")
+    else:
+        print()
+else:
+    print()
+play_game = input("Press <Enter> to play")
+while play_game == "":
+    print()
