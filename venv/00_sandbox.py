1c6b4444b18889415f692000ddca8bf778054f52
magno.franc <magno.franc@awatapu.school.nz>
2021-09-23 09:58:52 +1200

    experimenting

--- venv/00_sandbox.py
+++ venv/00_sandbox.py
@@ -1,25 +1,33 @@
-import time
-def countdown(t):
-    while t>0:
-        print(t)
-        t = t - 5
-        time.sleep(5)
-    print("Time's Up!")
+from time import *
+import threading
 
+def countdown():
+    global my_timer
+
+    my_timer = 20
+
+    for x in range(5):
+        my_timer = my_timer - 1
+        sleep(1)
+
+    print("Time's Up")
+
+countdown_thread = threading.Thread(target = countdown)
+
+countdown_thread.start()
+
+score = 0
 incorrect_answers = 0
 correct_answers = 0
+play_game = True
 
 answer_one = 'blue whale'
 answer_two = "mercury"
 answer_three = "netflix"
 answer_four = "china"
 
-seconds = 20
-
-seconds = int(seconds)
-while seconds > 0:
-    countdown(seconds)
-    while True:
+while my_timer > 0:
+    while play_game == True:
         answer = input("What is the biggest animal that has ever lived?")
         answer = answer.lower()
         if answer == answer_one:
@@ -29,7 +37,8 @@ while seconds > 0:
         else:
             print("INCORRECT. TRY AGAIN")
             incorrect_answers += 1
-    while True:
+
+    while play_game == True:
         answer = input("What is the smallest planet in our solar system?")
         answer = answer.lower()
         if answer == answer_two:
@@ -39,7 +48,8 @@ while seconds > 0:
         else:
             print("INCORRECT. TRY AGAIN")
             incorrect_answers += 1
-    while True:
+
+    while play_game == True:
         answer = input("What is the most popular streaming service?")
         answer = answer.lower()
         if answer == answer_three:
@@ -49,7 +59,8 @@ while seconds > 0:
         else:
             print("INCORRECT. TRY AGAIN")
             incorrect_answers += 1
-    while True:
+
+    while play_game == True:
         answer = input("Which country has the highest population?")
         answer = answer.lower()
         if answer == answer_four:
@@ -60,4 +71,8 @@ while seconds > 0:
             print("INCORRECT. TRY AGAIN")
             incorrect_answers += 1
 
+if my_timer == 0:
+    play_game == False
+
 print("You got {} correct answers, and {} incorrect guesses.".format(correct_answers, incorrect_answers))
+
