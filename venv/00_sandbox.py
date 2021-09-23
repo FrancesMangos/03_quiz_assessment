e7757b8e095333a8824eaafd73d9d0d627f067a5
magno.franc <magno.franc@awatapu.school.nz>
2021-09-24 10:56:53 +1200

    pain

--- venv/00_sandbox.py
+++ venv/00_sandbox.py
@@ -4,75 +4,54 @@ import threading
 def countdown():
     global my_timer
 
-    my_timer = 20
+    my_timer = 6
 
-    for x in range(5):
+    for x in range(6):
         my_timer = my_timer - 1
         sleep(1)
 
-    print("Time's Up")
+    print()
+
+answer_list = ["blue whale", "mercury", "netflix", "china"]
+
+question_list = ["What is the biggest animal that has ever lived?",
+                 "What is the smallest planet in our solar system?",
+                 "What is the most popular streaming service?",
+                 "Which country has the highest population?"]
+
+incorrect_answers = 0
+correct_answers = 0
 
 countdown_thread = threading.Thread(target = countdown)
 
 countdown_thread.start()
 
-score = 0
-incorrect_answers = 0
-correct_answers = 0
-play_game = True
-
-answer_one = 'blue whale'
-answer_two = "mercury"
-answer_three = "netflix"
-answer_four = "china"
-
 while my_timer > 0:
-    while play_game == True:
-        answer = input("What is the biggest animal that has ever lived?")
-        answer = answer.lower()
-        if answer == answer_one:
-            print("CORRECT!")
-            correct_answers += 1
-            break
-        else:
-            print("INCORRECT. TRY AGAIN")
-            incorrect_answers += 1
 
-    while play_game == True:
-        answer = input("What is the smallest planet in our solar system?")
-        answer = answer.lower()
-        if answer == answer_two:
-            print("CORRECT!")
-            correct_answers += 1
-            break
-        else:
-            print("INCORRECT. TRY AGAIN")
-            incorrect_answers += 1
+    print("QUESTION ONE")
+    print(question_list[0])
+    sleep(1.0)
+    answer = input("What is your answer?")
+    if answer == answer_list[0]:
+        print("Correct!")
+    else:
+        print("Incorrect!")
+    print()
 
-    while play_game == True:
-        answer = input("What is the most popular streaming service?")
-        answer = answer.lower()
-        if answer == answer_three:
-            print("CORRECT!")
-            correct_answers += 1
-            break
-        else:
-            print("INCORRECT. TRY AGAIN")
-            incorrect_answers += 1
+    if my_timer == 0:
+        break
 
-    while play_game == True:
-        answer = input("Which country has the highest population?")
-        answer = answer.lower()
-        if answer == answer_four:
-            print("CORRECT!")
-            correct_answers += 1
-            break
-        else:
-            print("INCORRECT. TRY AGAIN")
-            incorrect_answers += 1
+    print("QUESTION TWO")
+    print(question_list[1])
+    sleep(1.0)
+    answer_one = input("What is your answer?")
+    if answer_one == answer_list[1]:
+        print("Correct!")
+    else:
+        print("Incorrect!")
 
-if my_timer == 0:
-    play_game == False
+    if my_timer == 0:
+        break
 
-print("You got {} correct answers, and {} incorrect guesses.".format(correct_answers, incorrect_answers))
 
+print("Time's Up!")
