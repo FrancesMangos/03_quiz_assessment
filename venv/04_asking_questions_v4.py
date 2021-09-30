18b5dacad3850f16cc16e81292f8320d6ee330b9
magno.franc <magno.franc@awatapu.school.nz>
2021-10-01 09:17:05 +1300

    final program that now randomizes order of questions

---
+++ venv/04_asking_questions_v4.py
@@ -0,0 +1,30 @@
+play_game = True
+
+test_list = [["What is the most used streaming service?", "netflix", "disney+", "hulu"],
+             ["What is the smallest planet in our solar system?", "mercury", "venus", "earth"],
+             ["In which country did Lego originate from?", "denmark", "germany", "england"],
+             ["What chases the player's character in Pacman?", "ghosts", "goblins", "inner demons and self doubt"]]
+
+correct_answers = ["netflix", "mercury", "denmark", "ghosts"]
+
+question = 1
+
+while play_game == True:
+    print("Question {}".format(question))
+    print(test_list[0][0])
+    print("A. {}".format(test_list[0][1]))
+    print("B. {}".format(test_list[0][2]))
+    print("C. {}".format(test_list[0][3]))
+    guess = input("What is your answer?")
+    if guess in correct_answers:
+        print("CORRECT!")
+    else:
+        print("INCORRECT. :(")
+    del test_list[0]
+    question += 1
+    print()
+
+play_game = False
+
+if play_game == False:
+    print("Game Over")
