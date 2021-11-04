b196db71d9fae64fed1d5857668d27ab5b46a02b
magno.franc <magno.franc@awatapu.school.nz>
2021-11-05 10:55:17 +1300

    reworked to fit everything else - quiz list and starting parameters

--- venv/06_score_and_lives_v1.py
+++ venv/06_score_and_lives_v1.py
@@ -1,28 +1,29 @@
+import random
 play_game = True
 
-test_list = [["What is the most used streaming service?", "netflix", "disney+", "hulu"],
-             ["What is the smallest planet in our solar system?", "mercury", "venus", "earth"],
-             ["In which country did Lego originate from?", "denmark", "germany", "england"],
-             ["What chases the player's character in Pacman?", "ghosts", "goblins", "inner demons and self doubt"]]
+test_list = [["What is the most used streaming service?", "netflix", "disney+", "hulu", "a", "A"],
+             ["What is the smallest planet in our solar system?", "mercury", "venus", "earth", "a", "A"],
+             ["In which country did Lego originate from?", "germany", "england", "denmark", "c", "C"],
+             ["What chases the player's character in Pacman?", "goblins", "ghosts", "inner demons and self doubt", "b", "B"]]
 
-correct_answers = ["netflix", "mercury", "denmark", "ghosts"]
+random.shuffle(test_list)
 
 question = 1
 score = 0
 lives = 3
 questions_answered = 0
 
-while play_game == True:
+while play_game == True and len(test_list) != 0 and lives > 0:
     print("Question {}".format(question))
     print(test_list[0][0])
     print("A. {}".format(test_list[0][1]))
     print("B. {}".format(test_list[0][2]))
     print("C. {}".format(test_list[0][3]))
     guess = input("What is your answer?")
-    if guess in correct_answers:
+    if guess == test_list[0][4] or guess == test_list[0][5]:
         print("CORRECT!")
         score += 10
-        questions_answered + 1
+        questions_answered += 1
     else:
         print("INCORRECT!")
         lives -= 1
@@ -30,10 +31,12 @@ while play_game == True:
     del test_list[0]
     question += 1
     print()
-    if lives == 0:
-        play_game = False
 
-if play_game == False:
+if lives == 0:
     print("All Lives Lost. Game Over")
+    print("You answered {} questions correctly!".format(questions_answered))
+    print("Your final score is: {}".format(score))
+else:
+    print("Game Over")
     print("You answered {} questions correctly!".format(questions_answered))
     print("Your final score is: {}".format(score))
